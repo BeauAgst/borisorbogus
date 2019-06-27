34230f937f0ba66843204729b6d8654468052753
Beau August <beauagst@gmail.com>
2019-06-27 17:54:20 +0100

    Initial setup of assets

---
+++ src/components/GameTitle.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="game-title">
+    <div>
+      <span>Boris</span>
+    </div>
+    <div>
+      <span>or</span>
+    </div>
+    <div>
+      <span>Bogus</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameTitle',
+};
+</script>
+
+<style lang="scss">
+.game-title {
+  display: inline-block;
+  position: relative;
+  z-index: 1;
+  line-height: 1;
+  font-size: 8vw;
+  background: $color--primary;
+  border-radius: .1em;
+  box-shadow: 2px 1px 19px rgba(0, 0, 0, 0.15);
+
+  div {
+    display: inline-block;
+    vertical-align: top;
+    padding: 0.05em .2em;
+    border-radius: .2em;
+    letter-spacing: -0.01em;
+    line-height: 1;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: 900;
+    background: $color--primary;
+  }
+
+  span {
+    display: inline-block;
+    vertical-align: top;
+    line-height: 1;
+  }
+}
+
+@media (max-width: 767px) and (orientation: portrait) {
+  .game-title {
+    font-size: 10vw;
+
+    div {
+      display: block;
+    }
+  }
+}
+
+@media (min-width: 1200px) {
+  .game-title {
+    font-size: 96px;
+  }
+}
+</style>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,55 @@
+<template>
+  <footer>
+    <div class="made-by">
+      Made with &#10084; by
+      <template v-for="(contributor, index) in contributors">
+        <a
+          :href="contributor.url"
+          :key="contributor.name"
+          class="made-by__name"
+          rel="noopener noreferrer nofollow"
+          target="_blank"
+        >{{ contributor.name }}</a>
+        {{ index === contributors.length -1 ? null : '&' }}
+      </template>
+    </div>
+    <div class="other-links">
+        <a rel="noopener" target="_blank" href="https://voteforpolicies.org.uk">Vote for policies</a> |
+        <a href="#">Print-out Boris mask!</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'PageFooter',
+  data() {
+    return {
+      contributors: [
+        {
+          name: 'Beau August',
+          url: 'https://beaugust.co.uk',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+footer {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  text-align: center;
+  padding: 20px;
+  font-size: 13px;
+}
+
+@media (max-width: 767px) and (max-height: 639px) and (orientation: portrait) {
+  footer {
+    position: static;
+  }
+}
+</style>
